aa0b56d0d7f8b9dbd027ea7e80b07629c73d12e5
Stone-Red-Code <56473591+Stone-Red-Code@users.noreply.github.com>
2021-03-14 22:16:54 +0100

    Initial commit

---
+++ ImgurAutoUploader/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Imgur.API.Authentication;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ImgurAutoUploader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ApiClient apiClient;
+        private bool check = true;
+
+        private double width = 0;
+        private double height = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            apiClient = new ApiClient(".");
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage() && check)
+            {
+                check = false;
+                var image = Clipboard.GetImage();
+                if (width != image.Width || height != image.Height)
+                {
+                    width = image.Width;
+                    height = image.Height;
+                    new Popup(image, apiClient).ShowDialog();
+                }
+                check = true;
+            }
+        }
+    }
+}
---
+++ ImgurAutoUploader/Popup.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Imgur.API.Authentication;
+using Imgur.API.Endpoints;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace ImgurAutoUploader
+{
+    /// <summary>
+    /// Interaktionslogik für Popup.xaml
+    /// </summary>
+    public partial class Popup : Window
+    {
+        private BitmapSource bitmap;
+        private ApiClient apiClient;
+        private DispatcherTimer timer = new DispatcherTimer();
+        private bool upload = false;
+
+        public Popup(BitmapSource bitmapSource, ApiClient client)
+        {
+            InitializeComponent();
+            Rect desktopWorkingArea = SystemParameters.WorkArea;
+            Left = desktopWorkingArea.Right - Width;
+            Top = desktopWorkingArea.Bottom - Height;
+            imageBox.Source = bitmapSource;
+            bitmap = bitmapSource;
+            apiClient = client;
+
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            using (var fileStream = new FileStream("img.png", FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bitmap));
+                encoder.Save(fileStream);
+            }
+            Upload();
+        }
+
+        private void SecretButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private async void Upload()
+        {
+            upload = true;
+            UploadProgressBar.Visibility = Visibility.Visible;
+            SecretButton.Visibility = Visibility.Collapsed;
+            UploadButton.Visibility = Visibility.Collapsed;
+
+            HttpClient httpClient = new();
+
+            string filePath = @"img.png";
+            using var fileStream = File.OpenRead(filePath);
+
+            ImageEndpoint imageEndpoint = new ImageEndpoint(apiClient, httpClient);
+            var imageUpload = await imageEndpoint.UploadImageAsync(fileStream);
+
+            string link = imageUpload?.Link ?? "Error";
+            Clipboard.SetText(link);
+
+            UploadProgressBar.Visibility = Visibility.Collapsed;
+            LinkTextBlock.Visibility = Visibility.Visible;
+            LinkTextBlock.Text = imageUpload.Link;
+
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            DispatcherTimer timer = (DispatcherTimer)sender;
+            timer.Stop();
+            timer.Tick -= TimerTick;
+            Close();
+        }
+
+        private void Grid_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!upload)
+                timer.Stop();
+        }
+
+        private void Grid_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!upload)
+                timer.Start();
+        }
+    }
+}
